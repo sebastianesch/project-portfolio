41d0d7ce9e0efc8fce2b0117922b50838b480149
Sebastian Esch <seb.astian.esch@gmail.com>
2022-10-24 15:31:18 +0000

    Add local launchpad for testing

--- srv/server.js
+++ srv/server.js
@@ -5,4 +5,13 @@ cds.on('bootstrap', app => {
     app.use(proxy())
 })
 
+if (process.env.NODE_ENV !== 'production') {
+    const { cds_launchpad_plugin } = require('cds-launchpad-plugin');
+    cds.once('bootstrap', app => {
+        const handler = new cds_launchpad_plugin();
+        app.use(handler.setup({theme: 'sap_horizon'}))
+    })
+}
+
 module.exports = cds.server
+
